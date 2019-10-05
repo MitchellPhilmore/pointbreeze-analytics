9097bbd4bd81cbb589403186c99c39f973118e86
Nate A <natearzu@gmail.com>
2019-10-05 18:50:32 -0400

    Added azure cloud storage connector & requirements file.

---
+++ src/pointbreeze-analytics/README.txt
@@ -0,0 +1,4 @@
+To start Django service:
+1) cd src/pointbreeze-analytics/backend/
+2) python manage.py runserver
+3) Navigate to http://127.0.0.1:8000/admin
--- src/pointbreeze-analytics/backend/backend/urls.py
+++ src/pointbreeze-analytics/backend/backend/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from pointbreeze_analytics import views
 
 urlpatterns = [
+    path('/', views.index, name='landing'),
     path('admin/', admin.site.urls),
+    path('home/', views.home, name='login-home')
 ]
